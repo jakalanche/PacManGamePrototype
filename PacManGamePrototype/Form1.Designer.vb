42b6a27c3635d0475e3affff51fd25845aa9cf61
Jackson Beecher <jbeecher@ashcom.com.au>
2020-08-05 22:53:41 +1000

    User direct input with keys. Change animation on direction change. Timer check direction.

---
+++ PacManGamePrototype/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pacman = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pacman, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(224, 288)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'pacman
+        '
+        Me.pacman.Image = Global.PacManGamePrototype.My.Resources.Resources.pacman_eat
+        Me.pacman.Location = New System.Drawing.Point(41, 126)
+        Me.pacman.Name = "pacman"
+        Me.pacman.Size = New System.Drawing.Size(19, 18)
+        Me.pacman.TabIndex = 1
+        Me.pacman.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 288)
+        Me.Controls.Add(Me.pacman)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pacman, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents pacman As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ PacManGamePrototype/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Dim direction As String 'Current direction as string
+    Dim pacmanAnimation As String ' Current Animation eg. up, down, left right.
+    'Handle Movement using keys
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Up Then
+            direction = "up"
+        End If
+        If e.KeyCode = Keys.Down Then
+            direction = "down"
+        End If
+        If e.KeyCode = Keys.Left Then
+            direction = "left"
+        End If
+        If e.KeyCode = Keys.Right Then
+            direction = "right"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'PictureBox1.
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Handle 
+        If direction = "up" Then
+            pacman.Location = New Point(pacman.Location.X, pacman.Location.Y + 1)
+            If pacmanAnimation = "1" Then
+                'pacman.BackgroundImage = My.Resources.pacman_eat
+                'pacmanAnimation = "0"
+            Else
+                'pacman.BackgroundImage = My.Resources.pacman_eat1
+                'pacmanAnimation = "1"
+            End If
+        End If
+            If direction = "down" Then
+            pacman.Location = New Point(pacman.Location.X, pacman.Location.Y - 1)
+        End If
+        If direction = "left" Then
+            pacman.Location = New Point(pacman.Location.X - 1, pacman.Location.Y)
+        End If
+        If direction = "right" Then
+            pacman.Location = New Point(pacman.Location.X + 1, pacman.Location.Y)
+        End If
+    End Sub
+End Class
