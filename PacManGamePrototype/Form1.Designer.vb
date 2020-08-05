b8c690078a9601bfbb22c206640e7d3af85f79fc
Jackson Beecher <jbeecher@ashcom.com.au>
2020-08-06 00:30:03 +1000

    Animation and movement finished.

--- PacManGamePrototype/Form1.Designer.vb
+++ PacManGamePrototype/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,16 +20,30 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.pacman = New System.Windows.Forms.PictureBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pacman = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.pacman, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
+        '
+        'pacman
+        '
+        Me.pacman.Image = Global.PacManGamePrototype.My.Resources.Resources.pac_neutral
+        Me.pacman.Location = New System.Drawing.Point(104, 210)
+        Me.pacman.Name = "pacman"
+        Me.pacman.Size = New System.Drawing.Size(16, 16)
+        Me.pacman.TabIndex = 1
+        Me.pacman.TabStop = False
         '
         'PictureBox1
         '
@@ -41,19 +55,6 @@ Partial Class Form1
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'pacman
-        '
-        Me.pacman.Image = Global.PacManGamePrototype.My.Resources.Resources.pacman_right
-        Me.pacman.Location = New System.Drawing.Point(41, 126)
-        Me.pacman.Name = "pacman"
-        Me.pacman.Size = New System.Drawing.Size(16, 16)
-        Me.pacman.TabIndex = 1
-        Me.pacman.TabStop = False
-        '
-        'Timer1
-        '
-        Me.Timer1.Interval = 10
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -62,9 +63,9 @@ Partial Class Form1
         Me.Controls.Add(Me.pacman)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "Pacman"
         CType(Me.pacman, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
--- PacManGamePrototype/Form1.vb
+++ PacManGamePrototype/Form1.vb
@@ -3,19 +3,26 @@
     Dim pacmanAnimation As Integer = 0 ' Current Animation eg. up, down, left right. 0 - 5
     'Handle Movement using keys
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
-        MsgBox(e.KeyCode.ToString)
+        'MsgBox(e.KeyCode.ToString)
         If e.KeyCode = Keys.Up Then
             direction = "up"
+            pacmanAnimation = 1
         End If
         If e.KeyCode = Keys.Down Then
             direction = "down"
+            pacmanAnimation = 2
         End If
         If e.KeyCode = Keys.Left Then
             direction = "left"
+            pacmanAnimation = 3
         End If
         If e.KeyCode = Keys.Right Then
             direction = "right"
+            pacmanAnimation = 4
         End If
+        'If Check if possibel first
+        animationChange(pacmanAnimation)
+        'End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -25,35 +32,37 @@
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         'Handle 
         If direction = "up" Then
-            pacman.Location = New Point(pacman.Location.X, pacman.Location.Y + 1)
-            pacmanAnimation = 1
+            pacman.Location = New Point(pacman.Location.X, pacman.Location.Y - 1)
+
         End If
         If direction = "down" Then
-            pacman.Location = New Point(pacman.Location.X, pacman.Location.Y - 1)
-            pacmanAnimation = 2
+            pacman.Location = New Point(pacman.Location.X, pacman.Location.Y + 1)
+
         End If
         If direction = "left" Then
             pacman.Location = New Point(pacman.Location.X - 1, pacman.Location.Y)
-            pacmanAnimation = 3
+
         End If
         If direction = "right" Then
             pacman.Location = New Point(pacman.Location.X + 1, pacman.Location.Y)
-            pacmanAnimation = 4
+
         End If
-        animationChange(pacmanAnimation)
+
     End Sub
     Private Sub animationChange(pacAni)
         If pacAni = 1 Then 'up
-            pacman.BackgroundImage = My.Resources.pacman_up
+            pacman.Image = My.Resources.pacman_up
         ElseIf pacAni = 2 Then 'down
-            pacman.BackgroundImage = My.Resources.pacman_down
+            pacman.Image = My.Resources.pacman_down
         ElseIf pacAni = 3 Then 'left
-            pacman.BackgroundImage = My.Resources.pacman_left
+            pacman.Image = My.Resources.pacman_left
         ElseIf pacAni = 4 Then 'right
-            pacman.BackgroundImage = My.Resources.pacman_right
+            pacman.Image = My.Resources.pacman_right
         Else
-            pacman.BackgroundImage = My.Resources.pac_neutral
+            pacman.Image = My.Resources.pac_neutral
         End If
+        pacman.Refresh()
     End Sub
+
 
 End Class
